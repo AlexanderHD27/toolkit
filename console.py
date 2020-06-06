213bb468466d12c8c254eeafd85d2b1f344acc16
Alexander Wersching <alexander.wersching@t-online.de>
2020-06-06 12:46:44 +0200

    Added table funtcion

--- console.py
+++ console.py
@@ -31,3 +31,71 @@ def getcolorInt(fg, bg):
 
 def colored(text, fg, bg=colors.bg.BLACK):
     return fg + bg + str(text) + colors.RESET 
+
+def table(array: list, cellsize: int, style=1, direction=-1):
+
+    if type(array) != list:
+        raise ValueError("array has to be a list!")
+    if type(cellsize) != int:
+        raise ValueError("cellsize has to be a int!")
+
+    if len(array) <= 0:
+        if style == 1:
+            return "┌┐\n└┘\n"
+        elif style == 2:
+            return "┌┐\n└┘\n"
+        elif style == 0:
+            return "┌┐\n└┘\n"
+        else:
+            return ""
+    
+    if type(array[0]) != list:
+        if direction <= 0:
+            array = [array]
+        else:
+            array = [[i] for i in array]
+
+
+    if style == 1:
+        table = "┌" + "─"*len(array[0])*cellsize + (len(array[0])-1)*"─" + "┐\n"
+    elif style == 2:
+        table = "┌" + ("─"*cellsize + "┬")*len(array[0])
+        table = table[:-1] + "┐\n"
+    elif style == 0:
+        table = "┌" + "─"*len(array[0])*cellsize + "┐\n"
+    else:
+        table = ""
+ 
+
+    for x in range(len(array)):
+        if style >= 0:
+            table = table + "│"
+        
+        for y in range(len(array[x])):
+            table = table + str(array[x][y]).ljust(cellsize)[:cellsize]
+            if style == 1:
+                table = table + " "
+            elif style == 2:
+                table = table + "│"
+
+        if style == 1:
+            table = table[:-1] + "│\n"
+        elif style >= 0 and style != 2:
+            table = table + "│\n"
+        else:
+            table = table + "\n"
+            
+        if style == 2 and x+1 != len(array):
+            table = table + "├" + (len(array[0]))*("─"*cellsize + "┼")
+            table = table[:-1] + "┤\n"
+
+    if style == 1:
+        table = table + "└" + "─"*len(array[0])*cellsize + (len(array[0])-1)*"─" + "┘"
+    elif style == 2:
+        table = table + "└" + ("─"*cellsize + "┴")*len(array[0])
+        table = table[:-1] + "┘\n"
+    elif style == 0:
+        table = table + "└" + "─"*len(array[0])*cellsize + "┘"
+    
+    return table
+
