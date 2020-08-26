cf75420e65fc09aba7bc4cc5a1716a6d63406a5c
AlexanderHD27 <alexander.wersching@t-online.de>
2020-08-26 22:07:22 +0200

    Added x86-Assembler-Script

---
+++ assembl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import os
+import sys
+
+if len(sys.argv) < 2:
+    sys.stderr.write("Usage: <file> [<format>] [<loader>]\n")
+    sys.stderr.flush()
+    exit()
+
+if len(sys.argv) > 2:
+    FORMAT = sys.argv[2]
+else:
+    FORMAT = "elf32"
+
+if len(sys.argv) > 3:
+    LOADER = sys.argv[3]
+else:
+    LOADER = "elf_x86_64"
+
+
+name = ""
+
+for i in sys.argv[1].split(".")[:-1]:
+    name += i
+
+os.system("nasm -f elf64 {} -o {}.o".format(sys.argv[1], name)) 
+os.system("ld -m {} -o {} {}.o ".format(LOADER, name, name))
+os.system("rm {}.o".format(name))
